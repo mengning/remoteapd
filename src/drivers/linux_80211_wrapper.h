6c5c004f2022ec820aea4dbddd7ab5f81f23eaae
mengning <mengning@ustc.edu.cn>
2014-01-04 21:39:23 +0800

    简单梳理了一下netlink socket的调用顺序

--- src/drivers/linux_80211_wrapper.h
+++ src/drivers/linux_80211_wrapper.h
@@ -8,11 +8,29 @@
 
 #ifndef LINUX_80211_WRAPPER_H
 #define LINUX_80211_WRAPPER_H
-
+/*
+ * nl_cb = nl_cb_alloc(NL_CB_DEFAULT);
+ */
 struct nl_handle * nl_create_handle(struct nl_cb *cb, const char *dbg);
-void nl_destroy_handles(struct nl_handle **handle);
+/* 
+ * "nl80211" to id
+ * nl80211_id = genl_ctrl_resolve(global->nl, "nl80211"); 
+ * Resolve generic netlink family name to its identifier. 
+ * msg = nlmsg_alloc();
+ * genlmsg_put(msg, 0, 0, nl80211_id, 0, flags, cmd, 0);
+ * NLA_PUT_STRING(msg, CTRL_ATTR_FAMILY_NAME, "nl80211");
+ * 
+ */
+
 int nl_send_wrapper(struct nl_handle *handle, struct nl_msg *msg);
+/* 
+ * int process_event(struct nl_msg *msg, void *arg);
+ * nl_cb_set(nl_cb, NL_CB_VALID, NL_CB_CUSTOM,
+ *		  process_event, arg);
+ */
 int nl_recv_wrapper(struct nl_handle *handle, struct nl_cb *cb);
+void nl_destroy_handles(struct nl_handle **handle);
+
 /* 
  * callback:typedef void (*eloop_sock_handler)(int sock, void *eloop_data, void *user_data);
  */
